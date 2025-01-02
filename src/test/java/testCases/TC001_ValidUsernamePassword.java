e2c9a9df8a0234328d33897394d921f118b159eb
2021t01252 <chamodlakshitha666@gmail.com>
2025-01-02 22:20:17 +0530

    commit3

--- src/test/java/testCases/TC001_ValidUsernamePassword.java
+++ src/test/java/testCases/TC001_ValidUsernamePassword.java
@@ -1,4 +1,37 @@
 package testCases;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+import java.time.Duration;
 
 public class TC001_ValidUsernamePassword {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openPage(){
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+    }
+
+    @Test
+    public void TC001(){
+
+      HomePage homePage = new HomePage(driver);
+      homePage.clickAppoinmentBtn();
+
+    }
+
+    @Test
+    public void closeBrowser(){
+
+        //driver.quit();
+
+    }
 }
