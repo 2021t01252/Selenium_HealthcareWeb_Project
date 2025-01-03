fe79f68935d761dd254077dd283dc367a06c312f
2021t01252 <chamodlakshitha666@gmail.com>
2025-01-03 23:28:56 +0530

    commit5

--- src/main/java/pages/FormPage.java
+++ src/main/java/pages/FormPage.java
@@ -1,4 +1,39 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class FormPage {
+    WebDriver driver;
+    public FormPage(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath = "//input[@id='txt-username']")
+    WebElement usernameField;
+
+    @FindBy(xpath = "//input[@id='txt-password']")
+    WebElement passwordField;
+
+    @FindBy(xpath = "//button[@id='btn-login']")
+    WebElement loginButton;
+
+
+    public void submitForm(String userName, String password){
+        usernameField.sendKeys(userName);
+        passwordField.sendKeys(password);
+        loginButton.click();
+    }
+
+    public String getCurrentUrl(){
+        String currentUrl = driver.getCurrentUrl();
+        return currentUrl;
+    }
+
+
+
+
 }
--- src/test/java/testCases/TC001_ValidUsernamePassword.java
+++ src/test/java/testCases/TC001_ValidUsernamePassword.java
@@ -1,10 +1,12 @@
 package testCases;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.FormPage;
 
 import java.time.Duration;
 
@@ -26,6 +28,12 @@ public class TC001_ValidUsernamePassword {
 
       HomePage homePage = new HomePage(driver);
       homePage.clickAppoinmentBtn();
+      FormPage formPage = new FormPage(driver);
+      formPage.submitForm("John Doe","ThisIsNotAPassword");
+      String currentUrl = formPage.getCurrentUrl();
+      String expectedUrl = "https://katalon-demo-cura.herokuapp.com/#appointment";
+      Assert.assertEquals(currentUrl,expectedUrl,"UserName or Password Invalid");
+
 
     }
 
