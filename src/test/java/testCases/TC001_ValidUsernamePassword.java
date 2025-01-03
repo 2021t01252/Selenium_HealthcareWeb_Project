363dc575a363ebab3c44e25c17f149a9c1108ca9
2021t01252 <chamodlakshitha666@gmail.com>
2025-01-03 22:59:36 +0530

    commit4

--- src/test/java/testCases/TC001_ValidUsernamePassword.java
+++ src/test/java/testCases/TC001_ValidUsernamePassword.java
@@ -1,6 +1,7 @@
 package testCases;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
@@ -28,7 +29,7 @@ public class TC001_ValidUsernamePassword {
 
     }
 
-    @Test
+    @AfterMethod
     public void closeBrowser(){
 
         //driver.quit();
